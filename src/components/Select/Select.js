9b872c142a089515b42b505fe404d9b467491858
Craig Isesele <10642539+cmoihub@users.noreply.github.com>
2021-05-02 21:24:37 -0400

    update select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper value={value} onChange={onChange}>
       {children}
-    </select>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.select`
+  padding: 12px 16px;
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-family: Roboto;
+  font-size: 16px;
+  line-height: 19px;
+  border: none;
+
+  appearance: none;
+  display: block;
+  margin-right: 20px;
+  margin-bottom: 20px;
+
+  &:focus {
+    border: 2px solid ${COLORS.primary};
+    border-radius: 3px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
